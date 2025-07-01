cd7ef8bf76b85430a8f5235c0f65d5357d8dedc3
Ronja Koistinen <ronja.koistinen@helsinki.fi>
2025-07-01 15:33:40 +0300

    Local dev Cantaloupe in open proxy mode

--- local/docker/sshproxy-start.sh
+++ local/docker/sshproxy-start.sh
@@ -6,5 +6,9 @@ set -eu
 install -o 0 -g 0 -m 0600 /tmp/ssh_known_hosts /etc/ssh/
 install -o 0 -g 0 -m 0600 /tmp/ssh.conf /etc/ssh/ssh_config.d/
 
-exec /usr/bin/ssh -v -N -g -D "$SOCKS5_PROXY_PORT" -i /root/identity \
+printf "listen-address 0.0.0.0:8080\nforward-socks5 / localhost:$SOCKS5_PROXY_PORT .\n" \
+   > /etc/privoxy/config
+
+/usr/sbin/privoxy --no-daemon /etc/privoxy/config &
+/usr/bin/ssh -N -g -D "$SOCKS5_PROXY_PORT" -i /root/identity \
     -J "$SSH_JUMP_SEQUENCE" "$SOCKS5_PROXY_DST"
